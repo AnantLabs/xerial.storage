3ee8329f469df6862e3f76b4c9293ef20eb0cb05
leo <leo@localhost>
2008-01-16 02:39:46 +0000

    added a PostgreSQL support

--- src/main/java/org/xerial/db/sql/ConnectionPoolImpl.java
+++ src/main/java/org/xerial/db/sql/ConnectionPoolImpl.java
@@ -29,126 +29,178 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.LinkedList;
 
-import org.xerial.db.DBException;
 import org.xerial.db.DBErrorCode;
+import org.xerial.db.DBException;
 import org.xerial.util.StringUtil;
 
-
-
 /**
- * ConnectionPoolImpl class holds established JDBC connections.
- * For each time you get a connection, you must return it to the ConnectionPoolImpl
- * by calling returnConnection(connection). 
- *   
+ * ConnectionPoolImpl class holds established JDBC connections. For each time
+ * you get a connection, you must return it to the ConnectionPoolImpl by calling
+ * returnConnection(connection).
+ * 
  * @author leo
- *
+ * 
  */
-public class ConnectionPoolImpl implements ConnectionPool 
+public class ConnectionPoolImpl implements ConnectionPool
 {
-	private LinkedList<Connection> _connectionQueue = new LinkedList<Connection>();
-	private int _timeOutSecond = 10;
-	
-	private String _driver;
-	private String _address;
-	private int _numMaximumConnections = 5;
+    private LinkedList<Connection> _connectionQueue = new LinkedList<Connection>();
+    private int _timeOutSecond = 10;
 
-	
-	/**
-	 * Create an connection pool, in which only a single connection is pooled
-	 * This constructer is suited for memory database usage, which demands the same connection must always be returned to the user 
-	 * @param JDBCdriver	driver name: e.g. "org.sqlite.JDBC"
-	 * @param databaseAddress database address, e.g. "jdbc:sqlite:db.sqilte3"		
-	 * @throws DBException	when loading the driver or to establish connections fail 
-	 */
-	public ConnectionPoolImpl(String JDBCdriver, String databaseAddress) throws DBException
-	{
-	    this(JDBCdriver, databaseAddress, 1);
-	}
-	
-	/**
-	 * @param JDBCdriver	driver name: e.g. "org.sqlite.JDBC"
-	 * @param databaseAddress database address, e.g. "jdbc:sqlite:db.sqilte3"
-	 * @param the number of connections to keep  		
-	 * @throws DBException	when loading the driver or to establish connections fail 
-	 */
-	public ConnectionPoolImpl(String JDBCdriver, String databaseAddress, int numConnection) throws DBException
-	{
-		_driver = JDBCdriver;
-		_address = databaseAddress;
-		_numMaximumConnections = numConnection;
-		establishConnections();
-	}
-	
-	public synchronized void establishConnections() throws DBException
-	{
-		try {
-			Class.forName(_driver);
-		} catch (ClassNotFoundException e) {
-			throw new DBException(DBErrorCode.UnknownJDBCDriver, e);
-		}
-		
-		try
-		{
-			// establish the connections
-			for(int i=0; i<_numMaximumConnections; i++)
-			{
-				Connection connection = DriverManager.getConnection(_address);
-				_connectionQueue.add(connection);
-			}
-		}
-		catch(SQLException e)
-		{
-			throw new DBException(DBErrorCode.SQLiteDBFileNotFound, e);
-		}
-	}
-	
-	
-	
-	public synchronized void add(Connection establishedConnection)
-	{
-		_connectionQueue.add(establishedConnection);
-	}
-	
-	// @see org.xerial.db.ConnectionPool#getConnection()
-	public synchronized Connection getConnection() throws DBException
-	{
-		try {
-			while(_connectionQueue.isEmpty())
-			{
-				wait(_timeOutSecond*10);
-			}
-			Connection connection = _connectionQueue.getFirst();
-			_connectionQueue.removeFirst();
-			return connection;
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-			throw new DBException(DBErrorCode.ThreadInterruption, e);
-		}			
-	}
-	
-	// @see org.xerial.db.ConnectionPool#returnConnection(java.sql.Connection)
-	public synchronized void returnConnection(Connection leasedConnection) 
-	{
-		_connectionQueue.add(leasedConnection);
-		notify();
-	}
+    private String _driver;
+    private String _address;
+    private int _numMaximumConnections = 5;
 
-	// @see org.xerial.db.ConnectionPool#closeAll()
-	public synchronized void closeAll() throws DBException {
-		notifyAll();
-		LinkedList<String> exceptionList = new LinkedList<String>();
-		for(Connection c : _connectionQueue)
-		{
-			try {
-				c.close();
-			} catch (SQLException e) {
-				exceptionList.add(e.getMessage());
-			}
-		}
-		if(!exceptionList.isEmpty())
-			throw new DBException(DBErrorCode.FailureOnConnectionClose, StringUtil.join(exceptionList, ", "));
-	}
-	
-	
+    /**
+     * Create an connection pool, in which only a single connection is pooled
+     * This constructer is suited for memory database usage, which demands the
+     * same connection must always be returned to the user
+     * 
+     * @param JDBCdriver
+     *            driver name: e.g. "org.sqlite.JDBC"
+     * @param databaseAddress
+     *            database address, e.g. "jdbc:sqlite:db.sqilte3"
+     * @throws DBException
+     *             when loading the driver or to establish connections fail
+     */
+    public ConnectionPoolImpl(String JDBCDriver, String databaseAddress) throws DBException
+    {
+        this(JDBCDriver, databaseAddress, 1);
+    }
+
+    /**
+     * @param JDBCdriver
+     *            driver name: e.g. "org.sqlite.JDBC"
+     * @param databaseAddress
+     *            database address, e.g. "jdbc:sqlite:db.sqilte3"
+     * @param the
+     *            number of connections to keep
+     * @throws DBException
+     *             when loading the driver or to establish connections fail
+     */
+    public ConnectionPoolImpl(String JDBCDriver, String databaseAddress, int numConnection) throws DBException
+    {
+        _driver = JDBCDriver;
+        _address = databaseAddress;
+        _numMaximumConnections = numConnection;
+        establishConnections();
+    }
+
+    public ConnectionPoolImpl(String JDBCDriver, String databaseAddress, String user, String pass) throws DBException
+    {
+        this(JDBCDriver, databaseAddress, user, pass, 1);
+    }
+
+    public ConnectionPoolImpl(String JDBCDriver, String databaseAddress, String user, String pass, int numConnection)
+            throws DBException
+    {
+        _driver = JDBCDriver;
+        _address = databaseAddress;
+        _numMaximumConnections = numConnection;
+        establishConnections(user, pass);
+    }
+
+    public synchronized void establishConnections(String user, String pass) throws DBException
+    {
+        try
+        {
+            Class.forName(_driver);
+        }
+        catch (ClassNotFoundException e)
+        {
+            throw new DBException(DBErrorCode.UnknownJDBCDriver, e);
+        }
+
+        try
+        {
+            // establish the connections
+            for (int i = 0; i < _numMaximumConnections; i++)
+            {
+                Connection connection = DriverManager.getConnection(_address, user, pass);
+                _connectionQueue.add(connection);
+            }
+        }
+        catch (SQLException e)
+        {
+            throw new DBException(DBErrorCode.JDBCConnectionError, e);
+        }
+    }
+
+    public synchronized void establishConnections() throws DBException
+    {
+        try
+        {
+            Class.forName(_driver);
+        }
+        catch (ClassNotFoundException e)
+        {
+            throw new DBException(DBErrorCode.UnknownJDBCDriver, e);
+        }
+
+        try
+        {
+            // establish the connections
+            for (int i = 0; i < _numMaximumConnections; i++)
+            {
+                Connection connection = DriverManager.getConnection(_address);
+                _connectionQueue.add(connection);
+            }
+        }
+        catch (SQLException e)
+        {
+            throw new DBException(DBErrorCode.SQLiteDBFileNotFound, e);
+        }
+    }
+
+    public synchronized void add(Connection establishedConnection)
+    {
+        _connectionQueue.add(establishedConnection);
+    }
+
+    // @see org.xerial.db.ConnectionPool#getConnection()
+    public synchronized Connection getConnection() throws DBException
+    {
+        try
+        {
+            while (_connectionQueue.isEmpty())
+            {
+                wait(_timeOutSecond * 10);
+            }
+            Connection connection = _connectionQueue.getFirst();
+            _connectionQueue.removeFirst();
+            return connection;
+        }
+        catch (InterruptedException e)
+        {
+            e.printStackTrace();
+            throw new DBException(DBErrorCode.ThreadInterruption, e);
+        }
+    }
+
+    // @see org.xerial.db.ConnectionPool#returnConnection(java.sql.Connection)
+    public synchronized void returnConnection(Connection leasedConnection)
+    {
+        _connectionQueue.add(leasedConnection);
+        notify();
+    }
+
+    // @see org.xerial.db.ConnectionPool#closeAll()
+    public synchronized void closeAll() throws DBException
+    {
+        notifyAll();
+        LinkedList<String> exceptionList = new LinkedList<String>();
+        for (Connection c : _connectionQueue)
+        {
+            try
+            {
+                c.close();
+            }
+            catch (SQLException e)
+            {
+                exceptionList.add(e.getMessage());
+            }
+        }
+        if (!exceptionList.isEmpty())
+            throw new DBException(DBErrorCode.FailureOnConnectionClose, StringUtil.join(exceptionList, ", "));
+    }
+
 }
-
--- src/test/java/org/xerial/db/sql/sqlite/SQLiteAccessTest.java
+++ src/test/java/org/xerial/db/sql/sqlite/SQLiteAccessTest.java
@@ -24,6 +24,7 @@
 //--------------------------------------
 package org.xerial.db.sql.sqlite;
 
+import static org.junit.Assert.assertTrue;
 
 import java.util.List;
 
@@ -32,120 +33,103 @@ import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import static org.junit.Assert.*;
-
 import org.xerial.db.DBException;
 import org.xerial.db.Relation;
 import org.xerial.db.datatype.IntegerType;
 import org.xerial.db.datatype.StringType;
 import org.xerial.db.sql.ConnectionPool;
 import org.xerial.db.sql.ConnectionPoolImpl;
-import org.xerial.db.sql.DatabaseAccessBase;
 import org.xerial.db.sql.RelationBuilder;
-import org.xerial.db.sql.sqlite.SQLite;
-import org.xerial.db.sql.sqlite.SQLiteAccess;
 import org.xerial.util.CollectionUtil;
 import org.xerial.util.bean.BeanException;
 import org.xerial.util.log.Logger;
 
+public class SQLiteAccessTest
+{
 
+    static private ConnectionPool _connectionPool;
 
-public class SQLiteAccessTest {
+    static Logger _logger = Logger.getLogger(SQLiteAccessTest.class);
 
-	static private DatabaseAccessBase _dbAccess; 
-	static private ConnectionPool _connectionPool;
-	
-	static Logger _logger  = Logger.getLogger(SQLiteAccessTest.class);
-	
-	@BeforeClass
-	public static void openDatabase() throws DBException
-	{
+    @BeforeClass
+    public static void openDatabase() throws DBException
+    {
 
-	}
-	
-	@AfterClass
-	public static void closeDatabase() throws DBException
-	{
-	}
-	
-	
-	private SQLiteAccess query; 
-	
-	@Before
-	public void setUp() throws Exception {
-	    
+    }
+
+    @AfterClass
+    public static void closeDatabase() throws DBException
+    {}
+
+    private SQLiteAccess query;
+
+    @Before
+    public void setUp() throws Exception
+    {
+
         _connectionPool = new ConnectionPoolImpl(SQLite.driverName, SQLite.getMemoryDatabaseAddress());
-        _dbAccess = new DatabaseAccessBase(_connectionPool);
+        query = new SQLiteAccess(_connectionPool);
+        query.createTable("person", RelationBuilder.createRelation(Person.class));
+    }
 
-		query = new SQLiteAccess(_dbAccess);
-		query.createTable("person", RelationBuilder.createRelation(Person.class));
-	}
+    @After
+    public void tearDown() throws Exception
+    {
+        if (query != null)
+            query.dispose();
 
-	@After
-	public void tearDown() throws Exception {
-	    if(_dbAccess != null)
-	        _dbAccess.dispose();
+    }
 
-	}
+    @Test
+    public void getTableList() throws DBException
+    {
+        List<String> tableList = CollectionUtil.toString(query.getTableList());
 
-	
-	@Test
-	public void getTableList() throws DBException
-	{
-		List<String> tableList = CollectionUtil.toString(query.getTableList());
+        assertTrue(tableList.contains("person"));
+    }
 
-		assertTrue(tableList.contains("person"));
-	}
-	
-	@Test
-	public void hasTable() throws DBException
-	{
-		assertTrue(query.hasTable("person"));
-	}
-	
-	@Test
-	public void getRelation() throws DBException 
-	{
-		Relation r = query.getRelationSchema("person");
-		_logger.debug(r);
-	}
-	
-	
-	@Test
-	public void update() throws DBException, BeanException
-	{
-		query.insert(new Person(3, "leopard"), "person");
-		query.deleteByKeyValue(new Person(3), "person");
-	}
-	
-	@Test
-	public void createTable() throws DBException 
-	{
-		Relation r = new Relation();
-		r.add(new IntegerType("id", true, true));
-		r.add(new StringType("name"));
-		
-		query.createTable("tmp", r);
-		query.dropTable("tmp");
-	}
-	
-	
-	@Test
-	public void memoryDatabase() throws DBException, BeanException
-	{
-		ConnectionPool connectionPool = new ConnectionPoolImpl(SQLite.driverName, SQLite.getMemoryDatabaseAddress());
-		
-		DatabaseAccessBase dbAccess = new DatabaseAccessBase(connectionPool);
-		SQLiteAccess query = new SQLiteAccess(dbAccess);
-		
-		query.createTable("person", RelationBuilder.createRelation(Person.class));
-		query.insert(new Person(1, "leo"), "person");
-		query.insert(new Person(2, "yui"), "person");
+    @Test
+    public void hasTable() throws DBException
+    {
+        assertTrue(query.hasTable("person"));
+    }
 
-	}
-	
-}									
+    @Test
+    public void getRelation() throws DBException
+    {
+        Relation r = query.getRelationSchema("person");
+        _logger.debug(r);
+    }
 
+    @Test
+    public void update() throws DBException, BeanException
+    {
+        query.insert(new Person(3, "leopard"), "person");
+        query.deleteByKeyValue(new Person(3), "person");
+    }
 
+    @Test
+    public void createTable() throws DBException
+    {
+        Relation r = new Relation();
+        r.add(new IntegerType("id", true, true));
+        r.add(new StringType("name"));
 
+        query.createTable("tmp", r);
+        query.dropTable("tmp");
+    }
 
+    @Test
+    public void memoryDatabase() throws DBException, BeanException
+    {
+        ConnectionPool connectionPool = new ConnectionPoolImpl(SQLite.driverName, SQLite.getMemoryDatabaseAddress());
+        SQLiteAccess query = new SQLiteAccess(connectionPool);
+
+        query.createTable("person", RelationBuilder.createRelation(Person.class));
+        query.insert(new Person(1, "leo"), "person");
+        query.insert(new Person(2, "yui"), "person");
+
+        query.dispose();
+    }
+
+}
